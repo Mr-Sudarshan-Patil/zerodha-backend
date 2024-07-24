b6e2342090a3c38ce497dfdd6222ea31a5c2d41e
Sudarshan Patil <105053040+Mr-Sudarshan-Patil@users.noreply.github.com>
2024-07-23 21:33:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const corsConfig = {
   credential: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 }
-
+app.options("", cors(corsConfig)
 app.use(cors(corsConfig));
 app.use('/auth', authRouter)
 
