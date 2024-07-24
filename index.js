128ef17ca9779b92e5aa522e24bf507dffe4fc56
Sudarshan Patil <105053040+Mr-Sudarshan-Patil@users.noreply.github.com>
2024-07-23 22:22:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 app.use(bodyParser.json());
 
 const corsConfig = {
-  origin: "https://zerodha-backend.vercel.app",
+  origin: "",
   credential: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 }
